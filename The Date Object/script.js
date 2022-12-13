729ce77488d0bb66e55d7b5fa3210256c1daccd9
jonathan <jonathanbajoux.pro@gmail.com>
2022-12-13 14:33:46 +0100

    jonathan

--- The Date Object/script.js
+++ The Date Object/script.js
@@ -1,9 +1,48 @@
-var aryIannaTimeZones = ['Europe/Brussels','Atlantic/Reykjavik','America/Anchorage','Europe/Moscow'];
+/*var aryIannaTimeZones = ['Europe/Brussels','Atlantic/Reykjavik','America/Anchorage','Europe/Moscow'];
 
 let date = new Date;
 aryIannaTimeZones.forEach((timeZone) =>
 {
   let strTime = date.toLocaleString("en-US", {timeZone: `${timeZone}`});
   console.log(timeZone, strTime);
+});*/
+
+// exercice 1
+
+/*let date1 = new Date();
+
+let Brussels = date1.toLocaleString('fr-BE', {
+    timeZone: 'Europe/Brussels', year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'
 });
-console.log();
+document.getElementById('Brussels').innerHTML = 'Brussels : ' + Brussels;
+
+let Anchorage = date1.toLocaleString('en-US', {
+    timeZone: 'America/Anchorage', year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'
+});
+document.getElementById('Anchorage').innerHTML = 'Anchorage : ' + Anchorage;
+
+let Reykjavik = date1.toLocaleString('en-US',{
+    timeZone: 'Atlantic/Reykjavik',year: 'numeric',month: 'numeric',day: 'numeric',hour: 'numeric',minute: 'numeric',second: 'numeric'});
+document.getElementById('Reykjavik').innerHTML = 'Reykjavik : ' + Reykjavik;
+
+let Saint_Petersburg = date1.toLocaleString('ru-RU',{
+    timeZone: 'Europe/Moscow',year: 'numeric',month: 'numeric',day: 'numeric',hour: 'numeric',minute: 'numeric',second: 'numeric'});
+document.getElementById("Saint-Petersburg").innerHTML = 'Saint-Petersburg : ' + Saint_Petersburg;*/
+
+// exercice 2
+
+/*let Date_of_Birth = new Date('1987-05-12')
+let today = new Date()
+
+let date2 = ((today.getTime() - Date_of_Birth.getTime()) / (1000 * 60 * 60 * 24) + ' hours have passed betweeen')
+
+document.body.innerHTML = date2;*/
+
+// exercice 3
+
+/*var today = new Date();
+today.setHours(today.getHours() + 80000);
+document.body.innerHTML = today;*/
+
+// exercice 4
+
